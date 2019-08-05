07deea4fd06d483174b6d29d77a135a6dea832d2
zhen <kelly811124811124@gmail.com>
2019-08-06 00:09:34 +0800

    add liff to get profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  profile: LIFFUserProfile;
   title = 'liff-app';
+  ngOnInit() {
+    new Promise<LIFFUserProfile>(resolve => {
+      liff.init(data => {
+        // https://developers.line.biz/en/reference/liff/#initialize-liff-app
+        // data.language == "zh-TW"
+        // data.context.type = utou | room | group | none
+        // data.context.utouId
+        // data.context.roomId
+        // data.context.groupId
+        // data.context.userId
+        // data.context.viewType = compact | tall | full
+        resolve(liff.getProfile());
+      }, err => {
+        // LIFF initialization failed
+        // err.code;
+      });
+    })
+      .then((profile) => {
+        this.profile = profile;
+        // profile.userId
+        // profile.displayName
+        // profile.pictureUrl
+        // profile.statusMessage
+
+      });
+  }
 }
